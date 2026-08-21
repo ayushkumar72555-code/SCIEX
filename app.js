799df8aac949b7673e35a7a782548aea159bbccc
ayushk <ayushkumar72555@gmail.com>
2026-08-21 19:32:39 +0530

    Update field laboratory module label

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const moduleContainer = document.getElementById('moduleContainer');
 const modules = {
  physics:{name:'Interactive Physics Simulator',path:'physics/Interactive%20Physics%20Simulator/'},
  nbody:{name:'N-Body Gravitational Simulation',path:'physics/N-Body%20Gravitational%20Simulation/'},
- emlab:{name:'Electromagnetic Field Laboratory',path:'physics/Electromagnetic%20Field%20Laboratory/'},
+ emlab:{name:'Electric & Electromagnetic Field Laboratory',path:'physics/Electromagnetic%20Field%20Laboratory/'},
  solar:{name:'Solar System Explorer',path:'astronomy/Solar%20System%20Explorer/'},
  coordinates:{name:'Celestial Coordinate Simulator',path:'astronomy/Celestial%20Coordinate%20Simulator/'},
  blackhole:{name:'Black-Hole Visualization',path:'astrophysics/Black-Hole%20Visualization/'}
